8822e38905881844c394cd6840d2d4a61e937339
Matthew Walker <matsaleh13@gmail.com>
2018-11-21 11:00:08 -0600

    Rename for clarity, add const API.

--- src/std_allocator_adapter.h
+++ src/std_allocator_adapter.h
@@ -1,8 +1,10 @@
 /**
- * @file allocator_adapter.h
+ * @file std_allocator_adapter.h
  * @brief Adaptor to expose allok8or::Allocator<T> as std::allocator.
  */
 #pragma once
+
+#include "allocator.h"
 
 namespace allok8or {
 
@@ -10,14 +12,14 @@ namespace allok8or {
  * @brief Adapter class to expose allok8or::Allocator as std::allocator.
  *
  * Based on: https://howardhinnant.github.io/allocator_boilerplate.html
- *
+ * 
  * @tparam T Type of data to be allocated/deallocated.
  * @tparam TAllocator Type of the backing allocator to be passed to
- * allok8or::Allocator.
+ * allok8or::Allocator. NOTE: Must have const API.
  */
 template <typename T, typename TAllocator>
 class StdAllocatorAdapter {
-  Allocator<TAllocator>& m_allocator;
+  const Allocator<TAllocator>& m_allocator;
 
 public:
   using value_type = T;
@@ -42,20 +44,23 @@ public:
 
   StdAllocatorAdapter() = delete;
 
-  StdAllocatorAdapter(Allocator<TAllocator>& allocator) noexcept
+  StdAllocatorAdapter(const Allocator<TAllocator>& allocator) noexcept
       : m_allocator(allocator) {}
 
   template <typename U>
-  StdAllocatorAdapter(StdAllocatorAdapter<U> const&) noexcept {}
+  StdAllocatorAdapter(const StdAllocatorAdapter<U, TAllocator>& other) noexcept
+  : m_allocator(other.allocator()) {}
 
-  value_type* allocate(std::size_t n) {
+  value_type* allocate(std::size_t count) {
     return static_cast<value_type*>(
-        m_allocator.allocate(n * sizeof(value_type)));
+        m_allocator.allocate(count * sizeof(value_type)));
   }
 
-  void deallocate(value_type* p, std::size_t) noexcept {
-    m_allocator.deallocate(p);
+  void deallocate(value_type* user_data, std::size_t) noexcept {
+    m_allocator.deallocate(user_data);
   }
+
+  Allocator<TAllocator>& allocator() { return m_allocator; }
 
   const Allocator<TAllocator>& allocator() const { return m_allocator; }
 
@@ -87,7 +92,7 @@ public:
 
 template <typename T, typename U, typename TAllocator>
 bool operator==(StdAllocatorAdapter<T, TAllocator> const& lhs, StdAllocatorAdapter<U, TAllocator> const& rhs) noexcept {
-  return return lhs.allocator() == rhs.allocator();
+  return lhs.allocator() == rhs.allocator();
 }
 
 template <typename T, typename TAllocator, typename U>
