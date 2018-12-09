c3f60528269ff64134bedf96816983b9cc1c2255
Matthew Walker <matsaleh13@gmail.com>
2018-12-09 12:31:32 -0600

    Allocation stats reporter implementation.

--- src/diagnostic_allocation_stats_reporter.h
+++ src/diagnostic_allocation_stats_reporter.h
@@ -9,23 +9,72 @@
 #include "diagnostic_allocation_stats.h"
 
 // Library headers.
+#include <iostream>
 
-namespace allok8tor {
+namespace allok8or {
 namespace diagnostic {
 
+/**
+ * @brief Base class for reporting stats to an output stream.
+ *
+ * @tparam TImplementation Implementation class for specific output formats.
+ */
+template <typename TImplementation>
 class AllocationStatsReporter {
 public:
-  constexpr static void report_stats(const AllocationStatsTracker& stats);
+  constexpr void report_stats(const AllocationStatsTracker::StatsMap& stats,
+                              std::ostream& output) const {
+    impl().report_stats(stats, output);
+  }
+
+protected:
+  AllocationStatsReporter() {} // Don't create the base class.
 
 private:
+  constexpr TImplementation& impl() {
+    return *static_cast<TImplementation*>(this);
+  }
 
+  constexpr const TImplementation& impl() const {
+    return *static_cast<const TImplementation*>(this);
+  }
 };
 
-inline
-constexpr void AllocationStatsReporter::report_stats(const AllocationStatsTracker& stats) {
-  
+/**
+ * @brief AllocationStatsReporter that formats stats using comma-separated values (CSV).
+ * 
+ */
+class AllocationStatsCsvReporter
+    : public AllocationStatsReporter<AllocationStatsCsvReporter> {
+public:
+  void report_stats(const AllocationStatsTracker::StatsMap& stats,
+                              std::ostream& output) const;
+};
+
+inline void AllocationStatsCsvReporter::report_stats(
+    const AllocationStatsTracker::StatsMap& stats, std::ostream& output) const {
+      // Header row (column names)
+      output << "TypeName" << "," << "File" << "," << "Line" << ","
+             << "Allocs" << "," << "Alloc Bytes" << ","
+             << "Deallocs" << "," << "Dealloc Bytes" << ","
+             << "Net Allocs" << "," 
+             << "Net Alloc Bytes" << std::endl;
+             
+  for (const auto& stat_pair : stats) {
+    output << stat_pair.first.type_name << ","
+           << stat_pair.first.file_name << ","
+           << stat_pair.first.line << ","
+           << stat_pair.second.allocations << ","
+           << stat_pair.second.bytes_allocated << ","
+           << stat_pair.second.deallocations << ","
+           << stat_pair.second.bytes_deallocated << ","
+           << stat_pair.second.net_allocations() << ","
+           << stat_pair.second.net_bytes();
+  }
+
+  // Flush
+  output << std::endl;
 }
 
-
 } // namespace diagnostic
-} // namespace allok8tor
+} // namespace allok8or
